0690c2e9e0ba968853051e562cc579dd20a74cde
K Uyeda <kellikells@gmail.com>
2020-01-13 18:02:07 -0800

    stats are now displayed on html but not updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,106 +1,98 @@
-// document.ready = function () {
 
-// VARIABLES
-// ----------------------------------
-// starting game stats 
+
+// once the <body> loads, it runs this function
+function pageElementsFirst() {
+
+// variables for game stats
 let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
-let guessesSoFar = [" "];
+let guessesSoFar = [];
 
+// connect to HTML so stats are viewable
+let winsText = document.getElementById("wins");
+console.log(winsText);
+let lossesText = document.getElementById("losses");
+let guessesLeftText = document.getElementById("guesses-left");
+let guessesSoFarText = document.getElementById("guesses-so-far");
+console.log("hopefully this array displays ok" + guessesSoFarText);
 
-// array of possible computer choices
+// possible computer choices
 const computerKeyChoices = ["a", "b", "c", "d", "e", "f",
     "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const numberOfKeys = computerKeyChoices.length;
-console.log(numberOfKeys);
+// computer key choice stored in this variable
+let answer;
 
-// this is what the computer picks
-let compKey;
+// ---------------FUNCTION------
+//function to generate random computer choice in var, answer
+function generateAnswer() {
+    answer = computerKeyChoices[Math.floor(Math.random() * computerKeyChoices.length)];
+};
 
-// FUNCTIONS
-// ----------------------------------------------
+generateAnswer();
+        console.log("winning letter " + answer);
+// --------------Game starts once user presses a key-------------
+document.onkeyup = function(event) {
 
-function randomAnswer() {
-
-    let i = Math.floor(Math.random() * numberOfKeys);
-    
-    compKey = computerKeyChoices[i];
-    console.log("winning number " +compKey);
-}
-
-
-console.log(wins);
-console.log(losses);
-console.log(guessesLeft);
-console.log(guessesSoFar);
-
-
-
-// function to update stats: 
-function updateStats() {
-    document.getElementById()
-
-
-
-    document.querySelector("#wins").innerHTML =   wins;
-    document.querySelector("#losses").innerHTML =  losses;
-
-    document.querySelector("#guesses-left").innerHTML =  guessesLeft;
-
-    document.querySelector("#guesses-so-far").innerHTML =  guessesSoFar;
-}
-
-
-// // STARTING THE GAME ---- CALLING THE FUNCTIONS
-// // ---------------------------------------
-randomAnswer();
-// updateStats();
-
-
-
-
-
-// what happens when user presses a key 
-document.onkeyup = function (justAnything) {
-    // grabbing the key that was pressed during the keyPress
-
-    let userGuess = justAnything.key;
-    // userGuess = userGuess.toLowerCase;
-
-
-
-
+    // when key is pressed, stored this value in var, userGuess   
+    let userGuess = event.key;
     console.log(userGuess);
 
-    guessesLeft--;
-    console.log(guessesLeft);
+    // checking the key pressed is a letter key
+    if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
+   
 
-    if (userGuess === compKey && guessesLeft >= 0) {
-        wins++;
 
-        guessesLeft = 9;
-        // updateStats();
-        guessesSoFar = [];
-        console.log("I won " + wins );
-        randomAnswer();
 
+        // user wins 
+        if ((userGuess === answer) && (guessesLeft >= 0)) {
+            wins++;
+            console.log("i win " + wins);
+            guessesLeft = 9;
+            guessesSoFar = [];
+            console.log("should only contain this text" + guessesSoFar);
+            generateAnswer();
+
+
+        }
+
+        // user wrong 
+        else if ((userGuess !== answer) && (guessesLeft > 1)) {
+            guessesLeft--;
+            console.log("how many guesses I have left " + guessesLeft);
+            guessesSoFar.push(userGuess);
+            console.log(guessesSoFar);
+        }
+
+        // user loses
+        else {
+            losses++;
+            guessesLeft = 9;
+            guessesSoFar = [];
+            console.log("how many times i lost" + losses);
+            generateAnswer();
+        }
     }
-    else if (userGuess !== compKey && guessesLeft > 0) {
-        guessesSoFar.push(userGuess);
-        console.log("wrong answer");
-        // updateStats();
-    }
 
-    else {
-        losses++;
-        console.log("i lost " + losses);
-        guessesSoFar = [];
-        guessesLeft = 9;
-        randomAnswer();
-
-    }
 }
 
+  // html display
+  if (winsText) {
+      winsText.textContent = "Wins: " + wins;
+  }
+  if (lossesText) {
+      lossesText.textContent = "Losses: " + losses;
+  }
+  if (guessesLeftText) {
+      guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+  }
+  if (guessesSoFarText) {
+      guessesSoFarText.textContent = "Guesses So far: " + guessesSoFar;
+  }
+//   winsText.textContent = "Wins: " + wins;
+//   lossesText.textContent = "Losses: " + losses;
+//   guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+//   guessesSoFarText.textContent = "Guesses So far: " + guessesSoFarText;
+}
